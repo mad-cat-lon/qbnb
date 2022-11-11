f9ceede67e6646e30661c7b10d1d1fe1cb310270
b0ltzm4nn <19thcf@queensu.ca>
2022-11-11 15:25:25 -0500

    Sorted expected input and output files to ensure they match each other

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-QBNB ![pep-8](https://github.com/b0ltzm4nn/qbnb/actions/workflows/pep8.yml/badge.svg) ![tests](https://github.com/b0ltzm4nn/qbnb/actions/workflows/testing.yml/badge.svg)
+QBNB
+![pep-8](https://github.com/b0ltzm4nn/qbnb/actions/workflows/pep8.yml/badge.svg) ![tests](https://github.com/b0ltzm4nn/qbnb/actions/workflows/testing.yml/badge.svg)
--- qbnb_test/integration_tests/test_listing/test_create/test_create.py
+++ qbnb_test/integration_tests/test_listing/test_create/test_create.py
@@ -10,7 +10,7 @@ def test_r4_1_listing_create():
     test2 - Invalid title with space as prefix
     test3 - Invalid title with space as suffix
     test4 - Invalid title with non-alphanumeric chararacters
-    test5 - Valid title that conforms to r4-1
+    test5 - Valid title that conforms to R4-1
     """
     # Check if the test user has been created, stops one failed
     # test from making the next ones fail
@@ -29,10 +29,9 @@ def test_r4_1_listing_create():
         user = user[0]
     current_folder = Path(__file__).parent
     test_dir = current_folder / "r4_1"
-    expected_in_files = [x for x in test_dir.glob("*.in")]
-    expected_out_files = [x for x in test_dir.glob("*.out")]
+    expected_in_files = [x for x in test_dir.glob("*.in")].sort()
+    expected_out_files = [x for x in test_dir.glob("*.out")].sort()
     test_cases = dict(zip(expected_in_files, expected_out_files))
-    failures = []
     for in_file, out_file in test_cases.items():
         expected_in = open(in_file)
         expected_out = open(out_file).read()
@@ -45,9 +44,7 @@ def test_r4_1_listing_create():
         # Delete created listing
         listing = models.Listing.objects(owner=user)
         listing.delete()
-        if output.strip() != expected_out.strip():
-            failures.append(in_file.name)
-    assert failures == []
+        assert output.strip() == expected_out.strip()
     models.User.objects(email='r4@test.com', password='A123456a').delete()
 
 
@@ -74,10 +71,9 @@ def test_r4_2_listing_create():
         user = user[0]
     current_folder = Path(__file__).parent
     test_dir = current_folder / "r4_2"
-    expected_in_files = [x for x in test_dir.glob("*.in")]
-    expected_out_files = [x for x in test_dir.glob("*.out")]
+    expected_in_files = [x for x in test_dir.glob("*.in")].sort()
+    expected_out_files = [x for x in test_dir.glob("*.out")].sort()
     test_cases = dict(zip(expected_in_files, expected_out_files))
-    failures = []
     for in_file, out_file in test_cases.items():
         expected_in = open(in_file)
         expected_out = open(out_file).read()
@@ -90,9 +86,7 @@ def test_r4_2_listing_create():
         # Delete created listing
         listing = models.Listing.objects(owner=user)
         listing.delete()
-        if output.strip() != expected_out.strip():
-            failures.append(in_file.name)
-    assert failures == []
+        assert output.strip() == expected_out.strip()
     models.User.objects(email='r4@test.com', password='A123456a').delete()
 
 
@@ -101,7 +95,7 @@ def test_r4_3_listing_create():
     Boundary testing for R4-3
     test1 - Invalid description less than 20 characters
     test2 - Invalid description more than 2000 characters
-    test3 - Valid description with 20 characters 
+    test3 - Valid description with 20 characters
     test4 - Valid description with 2000 characters
     """
     # Check if the test user has been created, stops one failed
@@ -121,8 +115,8 @@ def test_r4_3_listing_create():
         user = user[0]
     current_folder = Path(__file__).parent
     test_dir = current_folder / "r4_3"
-    expected_in_files = [x for x in test_dir.glob("*.in")]
-    expected_out_files = [x for x in test_dir.glob("*.out")]
+    expected_in_files = [x for x in test_dir.glob("*.in")].sort()
+    expected_out_files = [x for x in test_dir.glob("*.out")].sort()
     test_cases = dict(zip(expected_in_files, expected_out_files))
     failures = []
     for in_file, out_file in test_cases.items():
@@ -137,13 +131,11 @@ def test_r4_3_listing_create():
         # Delete created listing
         listing = models.Listing.objects(owner=user)
         listing.delete()
-        if output.strip() != expected_out.strip():
-            failures.append(in_file.name)
-    assert failures == []
+        assert output.strip() == expected_out.strip()
     models.User.objects(email='r4@test.com', password='A123456a').delete()
 
 
-def test_r4_4_liting_create():
+def test_r4_4_listing_create():
     """
     Input partitioning for R4-4
     test1 - Invalid description is shorter than title
@@ -167,8 +159,8 @@ def test_r4_4_liting_create():
         user = user[0]
     current_folder = Path(__file__).parent
     test_dir = current_folder / "r4_4"
-    expected_in_files = [x for x in test_dir.glob("*.in")]
-    expected_out_files = [x for x in test_dir.glob("*.out")]
+    expected_in_files = [x for x in test_dir.glob("*.in")].sort()
+    expected_out_files = [x for x in test_dir.glob("*.out")].sort()
     test_cases = dict(zip(expected_in_files, expected_out_files))
     failures = []
     for in_file, out_file in test_cases.items():
@@ -183,9 +175,7 @@ def test_r4_4_liting_create():
         # Delete created listing
         listing = models.Listing.objects(owner=user)
         listing.delete()
-        if output.strip() != expected_out.strip():
-            failures.append(in_file.name)
-    assert failures == []
+        assert output.strip() == expected_out.strip()
     models.User.objects(email='r4@test.com', password='A123456a').delete()
 
 
@@ -214,10 +204,9 @@ def test_r4_5_listing_create():
         user = user[0]
     current_folder = Path(__file__).parent
     test_dir = current_folder / "r4_5"
-    expected_in_files = [x for x in test_dir.glob("*.in")]
-    expected_out_files = [x for x in test_dir.glob("*.out")]
+    expected_in_files = [x for x in test_dir.glob("*.in")].sort()
+    expected_out_files = [x for x in test_dir.glob("*.out")].sort()
     test_cases = dict(zip(expected_in_files, expected_out_files))
-    failures = []
     for in_file, out_file in test_cases.items():
         expected_in = open(in_file)
         expected_out = open(out_file).read()
@@ -230,9 +219,7 @@ def test_r4_5_listing_create():
         # Delete created listing
         listing = models.Listing.objects(owner=user)
         listing.delete()
-        if output.strip() != expected_out.strip():
-            failures.append(in_file.name)
-    assert failures == []
+        assert output.strip() == expected_out.strip()
     models.User.objects(email='r4@test.com', password='A123456a').delete()
 
 
@@ -259,10 +246,9 @@ def test_r4_8_listing_create():
         user = user[0]
     current_folder = Path(__file__).parent
     test_dir = current_folder / "r4_8"
-    expected_in_files = [x for x in test_dir.glob("*.in")]
-    expected_out_files = [x for x in test_dir.glob("*.out")]
+    expected_in_files = [x for x in test_dir.glob("*.in")].sort()
+    expected_out_files = [x for x in test_dir.glob("*.out")].sort()
     test_cases = dict(zip(expected_in_files, expected_out_files))
-    failures = []
     for in_file, out_file in test_cases.items():
         expected_in = open(in_file)
         expected_out = open(out_file).read()
@@ -271,11 +257,9 @@ def test_r4_8_listing_create():
             stdin=expected_in,
             capture_output=True,
             text=True
-        ).stdout
-        if output.strip() != expected_out.strip():
-            failures.append(in_file.name)
+        ).stdout  
+        assert output.strip() == expected_out.strip()
     # Delete created listing in first case
     listing = models.Listing.objects(owner=user)
     listing.delete()
-    assert failures == []
     models.User.objects(email='r4@test.com', password='A123456a').delete()
