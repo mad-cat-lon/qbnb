853b47817cd5273485a75eee0e1f6a0c045b3512
Adrian Chu <18anhc@queensu.ca>
2022-11-22 23:09:19 -0500

    Added new docker files, shipped application and system
    ## Description
    - added new files for setting up docker with mongodb
    - shipped application and system
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## Checklist
    
    - [x] My code follows PEP-8
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream modules

--- qbnb/__init__.py
+++ qbnb/__init__.py
@@ -3,8 +3,17 @@ init file for qbnb
 """
 from flask import Flask
 from flask_mongoengine import MongoEngine
+import os
 
 app = Flask(__name__)
+
+db_string = os.getenv('db_string')
+if db_string:
+    app.config['MONGODB_SETTINGS'] = db_string
+else:
+    app.config['MONGODB_SETTINGS'] = 'mongodb://localhost:27017/qbnb'
+app.app_context().push()
+
 app.config["MONGODB_SETTINGS"] = {
     "db": "qbnb",
     "host": "localhost",
